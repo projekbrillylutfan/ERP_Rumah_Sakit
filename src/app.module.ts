4313d43e4e56ddd6b00c21dddacf3ece88264931
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-07-02 22:38:59 +0700

    pembuatan role

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RoleGuard } from './role/role.guard';
 
 @Module({
   imports: [CommonModule, UserModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Auth } from 'src/common/auth.decorator';
+import { Roles } from 'src/role/role.decorator';
 
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  // @Get()
+  // @Roles(['PASIEN', 'ADMIN'])
+  // cekDulu(@Auth user: User): User {
+  //   return `hello ${user.peran} ${user.nama}`;
+  // }
+}
